a52c14374247c172222d8c237fab3b82caae144d
Erick Canali <erick.canali@gmail.com>
2021-03-21 17:47:55 -0300

    first 3 specifications added

--- cs50-network/network/models.py
+++ cs50-network/network/models.py
@@ -4,3 +4,48 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+# Vamos precisar armazenar as seguintes tabelas
+# Posts - user, content, timestamp, likes
+class Post(models.Model):
+    author = models.ForeignKey("User", on_delete=models.CASCADE, related_name="posts")
+    content = models.TextField(blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField("User", related_name="liked_posts")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "author": self.author,
+            "content": self.content,
+            "timestamp": self.timestamp.strftime("%b %d %Y, %I:%M %p"),
+            "likes": [user.id for user in self.likes.all()]
+        }
+
+
+# User (update) - incluir lista pessoas followers, lista pessoas following
+class Follower(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="followers")
+    follower = models.ForeignKey("User", on_delete=models.CASCADE, related_name="following")
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user": self.user,
+            "follower": self.follower
+        }
+
+
+# # Original / Initial
+# # User (update) - incluir lista pessoas followers, lista pessoas following
+# class Follower(models.Model):
+#     followers = models.ManyToManyField("User", related_name="followers")
+#     following = models.ManyToManyField("User", related_name="following")
+    
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "followers": [user.id for user in self.followers.all()],
+#             "following": [user.id for user in self.following.all()]
+#         }
--- cs50-network/network/views.py
+++ cs50-network/network/views.py
@@ -1,16 +1,91 @@
+from django import forms
+
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User, Post, Follower
+
+
+class CreatePostForm(forms.Form):
+    content = forms.CharField(widget=forms.Textarea(attrs={'placeholder': "What's happening?", "rows":2, 'class':'form-control'}), required=True, label='')
 
 
 def index(request):
-    return render(request, "network/index.html")
+    post_form = CreatePostForm(request.POST or None)
+    
+    if request.method == "POST":
+        if not request.user.is_authenticated:
+            # messages.error(request, 'Sorry, you must log in before create a listing.')
+            return HttpResponseRedirect(reverse("index"))
+
+        user = User.objects.get(id=request.user.id)
+
+        if post_form.is_valid():
+            post = Post(
+                author=user, 
+                content=post_form.cleaned_data["content"]
+            )
+            post.save()
+
+            return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "network/index.html", {
+        "post_form": post_form,
+        "posts": Post.objects.all().order_by('-id')
+    })
 
 
+def follow(request, user_id):
+    try:
+
+        # >>> INCLUIR VALIDAÇÃO BACK N DEIXAR PRÓPRIO USUÁRIO SE SEGUIR!
+
+        # get current user and target user to follow/unfollow
+        current_user = User.objects.get(id=request.user.id)
+        target_user = User.objects.get(id=user_id)
+
+        # remove if already following, else add the follower
+        if Follower.objects.filter(user=target_user, follower=current_user).exists():
+            Follower.objects.filter(user=target_user, follower=current_user).delete()
+        else:
+            add_follower = Follower(
+                user=target_user, 
+                follower=current_user
+            )
+            add_follower.save()
+    
+        return HttpResponseRedirect(reverse("profile", args=(target_user.username,)))
+    except User.DoesNotExist:
+        return HttpResponseRedirect(reverse("index"))
+
+
+def profile_view(request, username):
+    
+    # INCLUIR LÓGICA PARA USUÁRIO NÃO LOGADO ACESSAR PÁGINA
+    
+    # Query for requested user
+    try:
+        user = User.objects.get(username=username)
+        current_user = User.objects.get(id=request.user.id)
+        is_following = False
+
+        if Follower.objects.filter(user=user, follower=current_user).exists():
+            is_following = True
+
+        return render(request, "network/profile.html", {
+            "user_posts": Post.objects.filter(author_id=user).order_by('-id'),
+            "followers": len(Follower.objects.filter(user=user)),
+            "following": len(Follower.objects.filter(follower=user)),
+            "user_profile": user,
+            "is_following": is_following
+        })
+    except User.DoesNotExist:
+        return HttpResponseRedirect(reverse("index"))
+
+    
 def login_view(request):
     if request.method == "POST":
 
